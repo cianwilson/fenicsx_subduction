65995bcd041fde89286a26abedf9fc0aa3061be6
Cian Wilson <cwilson@carnegiescience.edu>
2024-07-19 17:19:06 -0400

    Some fixes for the time-dependent isoviscous subduction zone case.
    The main problem was that we hadn't initialized T_n to anything so it thought the previous temperature was 0 everywhere apart from where boundary conditions were applied.  I fixed this by updating the values of T_n at the beginning of the iteration, which also fixes it for all subsequent iterations.
    
    Other, problems included:
     * km was used in the crust when it should have been kc
     * (1 - theta) had been incorrectly deleted from the advection terms in fTw and fTs
    
    Some additional changes:
     * taking out superfluous parentheses to make it more readable and standardizing them between forms
     * allowing users to set the final time and timestep from the call to solve_time_isoviscous - they do this using units of Myr and code automatically scales them appropriately
     * users may also set theta from the function call
     * added some print statements to keep track of how many timesteps are expected to be taken
     * removing theta and dt as class parameters that are allowed to be set at __init__ or update stage, instead they must be set (to Constants) before getting the time dependent temperature forms

--- python/utils.py
+++ python/utils.py
@@ -10,6 +10,7 @@ except OSError:
     pass
 
 Myr_to_s = lambda a: a*1.e6*365.25*24*60*60
+s_to_Myr = lambda a: a/1.e6/365.25/24/60/60
 nondim_to_K = lambda T: T + 273.15
 mmpyr_to_mps = lambda v: v*1.0e-3/365.25/24/60/60
 mps_to_mmpyr = lambda v: v*1.0e3*365.25*24*60*60
